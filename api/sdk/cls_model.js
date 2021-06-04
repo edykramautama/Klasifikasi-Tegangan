d88ccd3959e56149359fcf3ddd655a49462da2bf
Edy Krama Utama <70458618+edykramautama@users.noreply.github.com>
2021-06-04 23:42:10 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -44,7 +44,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zarulrohmadhan/JST_Klasifikasi_BotTelegram/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/edykramautama/Klasifikasi-Tegangan/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
